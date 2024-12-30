7467a5027f015225c592e7cb085057849390e188
Aswin Jim Thuruthippilly <118459613+RealNickey@users.noreply.github.com>
2024-12-30 22:14:36 +0530

    Fix import path for LandingPage styles to match casing

--- frontend/src/LandingPage.jsx
+++ frontend/src/LandingPage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
-import "./styles/landingpage.css";
+import "./styles/LandingPage.css";
 
 const LandingPage = () => {
   const { loginWithRedirect } = useAuth0();
