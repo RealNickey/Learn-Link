5c37fd5210fbb49512534e8f4d99eed668df0dc2
Aswin Jim Thuruthippilly <118459613+RealNickey@users.noreply.github.com>
2024-12-30 22:05:16 +0530

    Remove unused Login and Logout button imports from App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import LoginButton from "./LoginButton";
-import LogoutButton from "./LogoutButton";
 import LandingPage from "./LandingPage";
 
 function App() {
