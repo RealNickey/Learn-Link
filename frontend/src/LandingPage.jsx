7bc709038fa3259615ae78bc1b78a892f8db98c5
Aswin Jim Thuruthippilly <118459613+RealNickey@users.noreply.github.com>
2025-01-06 23:10:29 +0530

    Enhance LandingPage button styles for improved visibility and user experience

--- frontend/src/LandingPage.jsx
+++ frontend/src/LandingPage.jsx
@@ -17,8 +17,15 @@ const LandingPage = () => {
       <h1>Welcome to Learn-Link</h1>
       <p>Your one-stop solution for learning and collaboration.</p>
       <div>
-        <button onClick={() => loginWithRedirect()} className="get-started-button">
-          Get Started
+        <br></br>
+        <button 
+          onClick={() => loginWithRedirect()} 
+          className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50"
+        >
+          <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+          <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
+               GET   STARTED    
+          </span>
         </button>
       </div>
     </div>
