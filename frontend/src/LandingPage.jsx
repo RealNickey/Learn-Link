d31b3c6142aac2aa2a410bd08e809c38d6cb06af
Aswin Jim Thuruthippilly <118459613+RealNickey@users.noreply.github.com>
2025-01-07 12:25:53 +0530

    Refactor button layout on LandingPage for improved readability

--- frontend/src/LandingPage.jsx
+++ frontend/src/LandingPage.jsx
@@ -18,13 +18,13 @@ const LandingPage = () => {
       <p>Your one-stop solution for learning and collaboration.</p>
       <div>
         <br></br>
-        <button 
-          onClick={() => loginWithRedirect()} 
+        <button
+          onClick={() => loginWithRedirect()}
           className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50"
         >
           <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
           <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
-               GET   STARTED    
+            GET STARTED
           </span>
         </button>
       </div>
